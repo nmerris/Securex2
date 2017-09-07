11c5e0da9b53ee3dc54e8ab7576bc5a1ce0ca775
nmerris <nate.merris@gmail.com>
2017-09-07 15:05:01 -0400

    change delete book route to redirect to list

--- src/main/java/byAJ/Securex/controllers/BookController.java
+++ src/main/java/byAJ/Securex/controllers/BookController.java
@@ -43,6 +43,6 @@ public class BookController {
     @RequestMapping("/delete/{id}")
     public String deleteBook(@PathVariable("id")int bookid){
         bookRepository.delete(bookid);
-        return "listbooks";
+        return "redirect:/books/list";
     }
 }
